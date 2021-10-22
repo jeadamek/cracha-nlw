839a3007ac510cc24b706a7334346c1ca7c8cddc
Jessica Adamek <78454317+jeadamek@users.noreply.github.com>
2021-10-21 22:54:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: "jeadamek",
+  youtube: "channel/UCF57zglQAVYOa-z0TS9BbMQ",
+  facebook: "jessica.galli.129",
+  instagram: "je.adamek",
+  twitter: ""
+}
+
+function changeSocialMediaLinks(){
+  for(let li of socialLinks.children){
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://www.${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userProfile.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
